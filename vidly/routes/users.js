377fdaff54ee7ada5d3c715f4ff2d59c544a8c6d
Will Morris <morriswr01@gmail.com>
2018-10-02 23:27:47 +0100

    jwt auth in user registration

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const { User, validateUser } = require('../models/user');
 
@@ -16,7 +18,9 @@ router.post('/', async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
-    res.send(_.pick(user, ['_id', 'name', 'email']));
+
+    const token = jwt.sign({ _id: user._id}, config.get('jwtPrivateKey'));
+    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
