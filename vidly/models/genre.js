21e7dfe60730dec4f9e9fc49603baaac8f432265
Will Morris <morriswr01@gmail.com>
2018-10-12 11:06:23 +0100

    Beautified genre.js

--- vidly/models/genre.js
+++ vidly/models/genre.js
@@ -1,22 +1,20 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const genreSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            minlength: 3,
-            maxlength: 255
-        }
+const genreSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 255
     }
-);
+});
 const Genre = mongoose.model('Genre', genreSchema);
 
-function validateGenre(genreName){
+function validateGenre(genreName) {
     //Check if given genre name meets criteria using Joi
     const schema = {
-        name: Joi.string().min(3).required(),//Joi syntax for our name field
+        name: Joi.string().min(3).required(), //Joi syntax for our name field
     }
     const result = Joi.validate(genreName, schema);
     return result;
